c02a2832dcc6eba83873edfe1fdca6e0f8d3061d
Janespot <janejunell777@gmail.com>
2025-12-31 17:34:30 -0500

    Unsupervised Learning Example - Principle Component Analysis

--- README.md
+++ README.md
@@ -1,3 +1,11 @@
 ### Iris Dataset
 
 ![Iris Plot](images/iris-dataset.png)
+
+### Supervised Learning Example - Simple Linear Regression
+
+![Iris Plot](images/supervised.png)
+
+### Unsupervised Learning Example - Principle Component Analysis
+
+![Iris Plot](images/unsupervised.png)
--- iris.py
+++ iris.py
@@ -105,3 +105,19 @@ yfit = model.predict(Xfit)
 plt.scatter(x, y)
 plt.plot(xfit, yfit)
 plt.show()
+
+# Estimator API Unsupervised learning example - Principle Component Analysis(PCA)
+from sklearn.decomposition import PCA
+
+model = PCA(n_components=2)
+print(model)
+
+model.fit(X_iris)
+
+X_2D = model.transform(X_iris)
+
+iris['PCA1'] = X_2D[:, 0]
+iris['PCA2'] = X_2D[:, 1]
+sns.lmplot(x = "PCA1", y = "PCA2", hue = 'species', data = iris, fit_reg = False)
+
+# plt.show()
